338877c5c43d0ca1dee0cad2c701cf1bac0a4dc3
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-04-26 14:35:42 +0200

    Zadanie 1 - testy jednostkowe

---
+++ Comarch20230427.Services/Sale/PriceCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace Comarch20230427.Services.Sale;
+
+public class PriceCalculator
+{
+    public decimal GetCarPrice(string carType)
+    {
+        string normalizedCarType = carType.Trim().ToLower();
+
+        decimal result = normalizedCarType switch
+        {
+            "car" => 100000,
+            "truck" => 200000,
+            "bus" => 300000,
+            _ => throw new ArgumentException("Podany typ auta jest nieprawidłowy", nameof(carType)),
+        };
+            
+        return result;
+    }
+}
---
+++ Tests/Unit/Comarch20230427.Services.Tests/Sale/PriceCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Comarch20230427.Services.Sale;
+namespace Comarch20230427.Services.Tests.Sale;
+
+public class PriceCalculatorTests
+{
+    [TestCase("car", 100000)]
+    [TestCase("Truck", 200000)]
+    [TestCase("BuS", 300000)]
+    public void GetCarPriceShouldReturnValidPriceForCarType(string carType, decimal expected)
+    {
+        //Arrange
+        PriceCalculator priceCalculator = new();
+
+        //Act
+        decimal actual = priceCalculator.GetCarPrice(carType);
+
+        //Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase("")]
+    [TestCase("FooBar")]
+    public void GetCarPriceShouldThrowExceptionForInvalidCarTypes(string carType)
+    {
+        //Arrange
+        PriceCalculator priceCalculator = new();
+
+        //Act
+        //Assert
+        Assert.Throws<ArgumentException>(() => priceCalculator.GetCarPrice(carType));
+    }
+}
